4ce91c5024b679c90525ab218e84999c1bb4a5ee
Hải Nguyễn <nvhai2408@gmail.com>
2023-11-09 01:35:16 +0700

    ditme

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT;
+const port = 3000;
 
 // connect to db
 require("./src/configs/db/index.db").connect();
