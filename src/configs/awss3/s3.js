e95b6d6cbd4f02aaeffb7b3097ccf011a2e95982
Hải Nguyễn <nvhai2408@gmail.com>
2023-11-12 17:49:07 +0700

    update cors of api upload photo

--- src/configs/awss3/s3.js
+++ src/configs/awss3/s3.js
@@ -6,4 +6,26 @@ const s3 = new AWS.S3({
   region: process.env.S3Region,
 });
 
+const corsParams = {
+  Bucket: process.env.S3BucketName,
+  CORSConfiguration: {
+    CORSRules: [
+      {
+        AllowedOrigins: ["*"], // Allow all domains to be accessed
+        AllowedHeaders: ["*"], // Allow all headers to be accessed
+        AllowedMethods: ["GET", "PUT", "POST", "DELETE"], // Allow all methods to be accessed
+        MaxAgeSeconds: 3000, // Time interval
+      },
+    ],
+  },
+};
+
+s3.putBucketCors(corsParams, function (err, data) {
+  if (err) {
+    console.log("Error setting CORS configuration: ", err);
+  } else {
+    console.log("CORS configuration set successfully: ", data);
+  }
+});
+
 module.exports = s3;
