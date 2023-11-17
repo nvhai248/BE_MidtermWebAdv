d8ed9f24a852064c142ab7ac251aa7730a17438c
nvhai248 <nvhai2408@gmail.com>
2023-11-17 15:08:53 +0700

    update ACAO

--- src/index.js
+++ src/index.js
@@ -25,6 +25,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Replace * with specific origin if needed
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 //implement routers
 require("./routers/index.router")(app);
 
